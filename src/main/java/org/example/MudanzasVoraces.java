33cacc12d171caeab6246e7abf1237d6613e492b
alejo9am <a.arenasmoreira@gmail.com>
2024-04-05 11:17:38 +0200

    version 1.0

---
+++ src/main/java/org/example/MudanzasVoraces.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MudanzasVoraces {
+    int[] c; //conjunto de candidatos
+    int[] o;
+
+    public void algoritmo(){
+        Arrays.sort(c);
+    }
+
+    public boolean factible(){
+        return true;
+    }
+
+    public int peso(){
+        for(int objeto : c){
+
+        }
+        return 1;
+    }
+
+}
